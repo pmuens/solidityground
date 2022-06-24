09edbafb28965ac40357b341cf2b4a665c27a4df
Philipp Muens <philipp@muens.io>
2022-06-24 17:51:56 +0200

    Format code

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-import { NFT } from "../src/NFT.sol";
+import {NFT} from "../src/NFT.sol";
 
 contract NFTScript is Script {
-  function run() public {
-    vm.startBroadcast();
+    function run() public {
+        vm.startBroadcast();
 
-    NFT nft = new NFT("NFT Tutorial", "TUT", "baseURI");
+        NFT nft = new NFT("NFT Tutorial", "TUT", "baseURI");
 
-    vm.stopBroadcast();
-  }
+        vm.stopBroadcast();
+    }
 }
